2202e814e8a700e83cd3d57d7bead8ea1bc52c22
Mikael Brevik <mikaelbre@gmail.com>
2019-01-21 21:10:52 +0100

    Adds filters for admin blogposts

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
-const fetch = require('node-fetch');
-const limit = 4;
+const fetch = require("node-fetch");
+const limit = 6;
+
+function hasTag(post, tag) {
+  const tags = post.virtuals.tags.map(i => i.slug);
+  return tags.includes(tag.toLowerCase());
+}
 
 module.exports = async (_, res) => {
   // Allow all to get latest posts.
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   try {
     const request = await fetch(
-      `https://medium.com/variant-as/latest?format=json&limit=${limit}`
+      `https://medium.com/variant-as/latest?format=json&limit=20`
     );
     const dataText = await request.text();
-    const data = dataText.replace('])}while(1);</x>', '');
-    return JSON.parse(data);
+    const data = dataText.replace("])}while(1);</x>", "");
+    const parsed = JSON.parse(data);
+
+    if (!parsed.success) {
+      return parsed;
+    }
+
+    return parsed.payload.posts
+      .filter(p => !hasTag(p, "variantdag") && !hasTag(p, "results"))
+      .slice(0, limit);
   } catch (e) {
     console.error(e);
     return {
       success: false,
-      error: 'Unable to get blog entries'
+      error: "Unable to get blog entries"
     };
   }
 };
