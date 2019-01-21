f08792bc7aefacbb82037e6f57644933a2a57591
Mikael Brevik <mikaelbre@gmail.com>
2019-01-21 21:32:52 +0100

    Rewrites code for now 2.0 lambdas

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 const fetch = require("node-fetch");
 const limit = 6;
+const url = `https://medium.com/variant-as/latest?format=json&limit=20`;
 
 function hasTag(post, tag) {
   const tags = post.virtuals.tags.map(i => i.slug);
   return tags.includes(tag.toLowerCase());
 }
 
-module.exports = async (_, res) => {
-  // Allow all to get latest posts.
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
+async function getData() {
   try {
-    const request = await fetch(
-      `https://medium.com/variant-as/latest?format=json&limit=20`
-    );
+    const request = await fetch(url);
     const dataText = await request.text();
     const data = dataText.replace("])}while(1);</x>", "");
     const parsed = JSON.parse(data);
@@ -32,4 +28,14 @@ module.exports = async (_, res) => {
       error: "Unable to get blog entries"
     };
   }
+}
+
+module.exports = (_, res) => {
+  // Allow all to get latest posts.
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  getData().then(function(data) {
+    res.writeHeader(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(data));
+  });
 };
